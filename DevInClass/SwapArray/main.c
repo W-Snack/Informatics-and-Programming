f39ed1b62c7c7b61c72df71983d02dfdc2395028
W-Snack <ase3938@gmail.com>
2021-10-14 17:42:10 +0300

    SwapArray

--- DevInClass/SwapArray/main.c
+++ DevInClass/SwapArray/main.c
@@ -20,65 +20,49 @@ int main() {
 	scanf_s("%d", &method);
 	printf("Input size of the array\n");
 	scanf_s("%d", &size);
+
+	array = (int*)malloc(size * sizeof(int));
+	if (!array) {
+		printf("Error. Required memory is not allocated\n");
+		exit(1);
+	}
+
+	for (int i = 0; i < size; i++) {
+		array[i] = 0;
+	}
+
+	array_reverse = (int*)malloc(size * sizeof(int));
+	if (!array_reverse) {
+		printf("Error. Required memory is not allocated\n");
+		exit(1);
+	}
+	for (int i = 0; i < size; i++) {
+		array_reverse[i] = 0;
+	}
+
+	printf("Input array\n");
+	for (int i = 0; i < size; i++) {
+		scanf_s("%d", &el1);
+		array[i] = el1;
+	}
+
 	switch (method)
 	{
 	case 1:
-		array = (int*)malloc(size * sizeof(int));
-		array_reverse = (int*)malloc(size * sizeof(int));
-		if (!array || !array_reverse) {
-			printf("Error. Required memory is not allocated\n");
-			exit(1);
-		}
-		for (int i = 0; i < size; i++) {
-			array_reverse[i] = 0;
-			array[i] = 0;
-		}
-		printf("Input array\n");
-		for (int i = 0; i < size; i++) {
-			scanf_s("%d", &el1);
-			array[i] = el1;
-		}
 		for (int i = size / 2, j = 0; i < size; i++, j++) {
 			array_reverse[j] = array[i];
 		}
 		for (int i = 0, j = size / 2 + size % 2; j < size; i++, j++) {
 			array_reverse[j] = array[i];
 		}
-		printf("The resulting array: ");
-		for (int i = 0; i < size; i++) {
-			printf("%d ", array_reverse[i]);
-		}
-		free(array);
-		free(array_reverse);
 		break;
 	case 2:
-		array = (int*)malloc(size * sizeof(int));
-
-		if (!array) {
-			printf("Error. Required memory is not allocated\n");
-			exit(1);
+		for (int i = 0, j = size / 2; j < size; i++, j++) {
+			tmp = array[i];
+			array[i] = array[j];
+			array[j] = tmp;
 		}
-		for (int i = 0; i < size; i++) {
-			array[i] = 0;
-		}
-		printf("Input array\n");
-		for (int i = 0; i < size; i++) {
-			scanf_s("%d", &el);
-			array[i] = el;
-		}
-		if (size % 2 == 0) {
-			for (int i = 0, j = size / 2; j < size; i++, j++) {
-				tmp = array[i];
-				array[i] = array[j];
-				array[j] = tmp;
-			}
-		}
-		else {
-			for (int i = 0, j = size / 2; j < size; i++, j++) {
-				tmp = array[i];
-				array[i] = array[j];
-				array[j] = tmp;
-			}
+		if(size%2!=0){
 			array[size - 1] = tmp;
 			for (int i = size - 1; i >= size / 2 + 1; i--)
 			{
@@ -86,12 +70,18 @@ int main() {
 			}
 			array[size / 2 + 1] = tmp;
 		}
-		printf("The resulting array: ");
-		for (int i = 0; i < size; i++) {
-			printf("%d ", array[i]);
-		}
-		free(array);
 		break;
 	}
+	printf("The resulting array: ");
+	for (int i = 0; i < size; i++) {
+		if (method == 2) {
+			printf("%d ", array[i]);
+		}
+		else {
+			printf("%d ", array_reverse[i]);
+		}
+	}
+	free(array);
+	free(array_reverse);
 	return 0;
 }
