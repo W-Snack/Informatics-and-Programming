b441efee63574a300ba9d0260a8ac4d6f1132cdd
W-Snack <ase3938@gmail.com>
2021-10-15 10:07:35 +0300

    VerificationTask Variant_3 (not working enough)

---
+++ DevInClass/VerificationTask/main.c
@@ -0,0 +1,60 @@
+// Copyright 2021 FirstName LastName
+
+/*
+* Население некоторого маленького города N человек. Каждый человек написал информацию о том, с каким количеством людей он знаком. После сбора информации она должна быть проанализирована и должны быть выданы ответы:
+
+правдива ли информация,
+предложить вариант того, кто с кем знаком.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	int N, count_f, count_lot;
+	count_lot = 0;
+	int is_true = 1;
+	int* friends;
+	scanf_s("%d", &N);
+	friends = (int*)malloc(N * sizeof(int));
+	while (count_lot<N)
+	{
+		char ch;
+		int a;
+		int i = 0;
+		do {
+			scanf_s("%c", &ch);
+			if (ch >= 48 && ch <= 57) {
+				a = ch - '0';
+				friends[i] = a;
+				i++;
+				count_lot++;
+			}
+			else continue;
+		} while (ch != '\n');
+	}
+	for (int i = 0; i < N; i++) {
+		if (friends[i] >= N) {
+			is_true = 0;
+		}
+	}
+	if (!is_true) {
+		printf("False information\n");
+	}
+	else {
+		srand(time(NULL));
+		printf("A possible dating option:\nHuman : his acquaintances\n");
+		for (int i = 1; i <= N; i++) {
+			int j = 0;
+			printf("%d : ", i);
+			while (j < friends[i - 1]) {
+				printf("%d,", rand() % 5);
+				j++;
+			}
+			printf("\n");
+		}
+	}
+
+	free(friends);
+	return 0;
+}
