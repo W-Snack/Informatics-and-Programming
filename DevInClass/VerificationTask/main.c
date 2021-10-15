0af95dd188cfccdbc7b5ab1a6ff9db6db37bdd96
W-Snack <ase3938@gmail.com>
2021-10-15 10:55:01 +0300

    Solution for verifivcation task variant-3

--- DevInClass/VerificationTask/main.c
+++ DevInClass/VerificationTask/main.c
@@ -15,8 +15,19 @@ int main() {
 	count_lot = 0;
 	int is_true = 1;
 	int* friends;
+	int* people;
 	scanf_s("%d", &N);
 	friends = (int*)malloc(N * sizeof(int));
+	people = (int*)malloc(N * sizeof(int));
+	srand(time(NULL));
+
+	for (int i = 0; i < N; ++i)
+	{
+		people[i] = i+1;
+		
+	}
+
+
 	while (count_lot<N)
 	{
 		char ch;
@@ -42,19 +53,29 @@ int main() {
 		printf("False information\n");
 	}
 	else {
-		srand(time(NULL));
 		printf("A possible dating option:\nHuman : his acquaintances\n");
 		for (int i = 1; i <= N; i++) {
 			int j = 0;
+			srand(time(NULL));
+
+			// реализация алгоритма перестановки
+			for (int i = N - 1; i >= 1; i--)
+			{
+				int j = rand() % (i + 1);
+
+				int tmp = people[j];
+				people[j] = people[i];
+				people[i] = tmp;
+			}
 			printf("%d : ", i);
 			while (j < friends[i - 1]) {
-				printf("%d,", rand() % 5);
+					printf("%d ", people[j]);
 				j++;
 			}
 			printf("\n");
 		}
 	}
-
+	free(people);
 	free(friends);
 	return 0;
 }
